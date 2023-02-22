ad15dd026c76616481fe22ecacec4ad682b78d26
Iurii_Rotari <117731128+iuriiarbeitet@users.noreply.github.com>
2023-02-22 12:17:52 +0100

    Add files via upload

---
+++ Spirali_HW.java
@@ -0,0 +1,54 @@
+package HomeWork_Tasks;
+
+import java.util.Arrays;
+
+public class Spirali_HW {
+          public static void main(String[] args) {
+            zmei(7, 7);
+
+        }
+
+        public static void zmei(int h, int w) {
+            int[][] arr = new int[h][w];
+            int num = 1;
+            int flag = 0;
+            if (h < w) flag= h;
+            else flag = w;
+
+            int count = -1;
+            for (int x = 0; x <= flag/2; x++) {
+                count++;
+                   for (int i = count; i < arr.length-count ; i++) {
+                    if (i == count) {
+                    for (int j = count; j < arr[i].length-count; j++) {
+                        arr[i][j] = num++;
+                    }
+                    }
+                    for (int j = 0; j < arr[i].length; j++) {
+                        if (i > count && i < arr.length - 1-count  && j == arr[i].length - 1-count) {
+                            arr[i][j] = num++;
+                        }
+                    }
+                       if(count == arr.length/2) break;
+                    if (i == arr.length-1-count) {
+                        for (int j = count; j < arr[i].length-count; j++) {
+                            arr[i][arr[i].length - 1 - j] = num++;
+                        }
+                    }
+                    if (i == arr.length - 1-count) {
+                        for (int k = 1; k < arr.length-1-count-count; k++) {
+                            arr[arr.length-1-k-count][count] = num++;
+                            if(count == arr[i].length/2) break;
+                        }
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(Arrays.toString(arr[i]));
+            }
+        }
+    }
+
+
