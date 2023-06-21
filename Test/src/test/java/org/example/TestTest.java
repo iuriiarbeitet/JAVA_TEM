994e60ae25dc639f52a5b1d20e4f9ad32b078adb
Iurii Rotari <iurii.arbeitet@gmail.com>
2023-06-21 13:54:23 +0200

    minesweeper

---
+++ Test/src/test/java/org/example/TestTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+
+    @Test
+    @ExtendWith(MockitoExtension.class)
+    void add (){
+
+    }
+
+}
